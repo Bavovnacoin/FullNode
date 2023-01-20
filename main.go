7b5a6db4c29f75247b30b6006ec6bb4a641c8765
nazarevsky <nazarevsky.ua@gmail.com>
2023-01-20 19:05:47 +0200

    Generating difficulty, target and bits conversion

--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "bavovnacoin/mining"
-
 func main() {
-	mining.BitsToTarget("cb04041b")
+
 }
--- mining/difficulty.go
+++ mining/difficulty.go
@@ -1,11 +1,29 @@
 package mining
 
 import (
+	"fmt"
+	"math"
 	"math/big"
+	"strconv"
+	"strings"
+	"time"
 )
 
-func GenBits() {
+// Difficulty changes every 24h
+func GenBits(frstBlockTime time.Time, secBlockTime time.Time, bits string) {
+	spentTimeSec := secBlockTime.Unix() - frstBlockTime.Unix()
+	expextTimeSec := 24 * 60 * 60 // expected time for one day change
+	coef := float64(expextTimeSec) / float64(spentTimeSec)
+	target := BitsToTarget(bits)
+	target = target.Mul(target, big.NewInt(int64(coef*100)))
+	target = target.Div(target, big.NewInt(100))
+	targetStr := fmt.Sprintf("%x", target)
 
+	if len(targetStr)%2 != 0 {
+		targetStr = "0" + targetStr
+	}
+	targetNum, _ := new(big.Int).SetString(targetStr[4:6]+targetStr[:4]+strings.Repeat("0", len(targetStr)-6), 16)
+	println(TargetToBits(targetNum))
 }
 
 func BitsToTarget(bits string) *big.Int {
@@ -18,4 +36,22 @@ func BitsToTarget(bits string) *big.Int {
 	target, _ := new(big.Int).SetString(bits[2:6]+bits[:2], 16)
 	target.Mul(target, shift)
 	return target
+}
+
+func TargetToBits(target *big.Int) string {
+	targetStr := fmt.Sprintf("%x", target)
+	var targetShift string
+	var valStr string
+	if len(targetShift)%2 != 0 {
+		targetShift = targetStr[5:]
+		valStr = targetStr[3:5] + "0" + targetStr[:3]
+	} else {
+		targetShift = targetStr[6:]
+		valStr = targetStr[4:6] + targetStr[:4]
+	}
+	shiftVal, _ := big.NewInt(0).SetString("1"+targetShift, 16)
+	shiftValFloat, _ := strconv.ParseFloat(shiftVal.String(), 64)
+
+	shift := fmt.Sprintf("%x", (int(math.Log2(shiftValFloat)/8))+3)
+	return valStr + shift
 }
