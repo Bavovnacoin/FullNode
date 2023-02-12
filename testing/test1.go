988f2deae97a32d34e4047979448efa18c5ba730
nazarevsky <nazarevsky.ua@gmail.com>
2023-02-13 01:51:45 +0200

    Txo saving in db

--- testing/test1.go
+++ testing/test1.go
@@ -3,17 +3,18 @@ package testing
 import (
 	"bavovnacoin/account"
 	"bavovnacoin/blockchain"
+	"bavovnacoin/dbController"
 	"bavovnacoin/ecdsa"
 	"bavovnacoin/node_controller"
 	"bavovnacoin/node_controller/command_executor"
+	"bavovnacoin/utxo"
 )
 
 func process() {
-	blockchain.Database.OpenDb()
-	defer blockchain.Database.CloseDb()
-	defer blockchain.WriteMempoolData()
 	blockchain.RestoreMempool()
+	utxo.RestoreCoinDatabase()
 	blockchain.InitBlockchain()
+
 	var genesisAccKeyPair []ecdsa.KeyPair
 	genesisAccKeyPair = append(genesisAccKeyPair, ecdsa.KeyPair{PrivKey: "d966fded26f23d50bb1223cdc6efe4cfebc9f2d6967cb570122c040baf5d42091953a2ba6466963351a4c6bc616e1858de87de02724cc89d9306a62b6d29fab6",
 		PublKey: "033361587c679cf9476949cb7cdd15c07d6f2f9674886333f667bfedb87635a4b4"})
@@ -28,6 +29,9 @@ func process() {
 }
 
 func Test1() {
+	dbController.DB.OpenDb()
 	go process()
 	node_controller.CommandHandler()
+	blockchain.WriteMempoolData()
+	dbController.DB.CloseDb()
 }
