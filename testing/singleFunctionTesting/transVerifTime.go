425e3e458167ec06cb20d095bc4bfd917d65249c
nazarevsky <nazarevsky.ua@gmail.com>
2023-03-16 14:38:03 +0200

    Transaction time verification test

--- testing/singleFunctionTesting/transVerifTime.go
+++ testing/singleFunctionTesting/transVerifTime.go
@@ -3,6 +3,8 @@ package singleFunctionTesting
 import (
 	"bavovnacoin/account"
 	"bavovnacoin/dbController"
+	"bavovnacoin/ecdsa"
+	"bavovnacoin/hashing"
 	"bavovnacoin/testing"
 	"bavovnacoin/transaction"
 	"bavovnacoin/txo"
@@ -17,12 +19,51 @@ type TransVerifTime struct {
 	random *rand.Rand
 }
 
-/*
-Test creates a predefined ammount of transactions with a predefined
-ammount of incorrect transactions and vereficated them.
+// Same function as in transaction package but with time checking
+func VerifyTransactionTime(tx transaction.Transaction) (bool, []time.Duration) {
+	var timeFunConsumed []time.Duration
+	if tx.Version == 0 {
+		ecdsa.InitValues()
 
-The result is printed to the console.
-*/
+		var inpValue uint64
+		var outValue uint64
+		hashMesOfTx := hashing.SHA1(transaction.GetCatTxFields(tx))
+
+		// Checking signatures and unique inputs
+		for i := 0; i < len(tx.Inputs); i++ {
+			if len(tx.Inputs[i].ScriptSig.ToHexString()) < 66 {
+				return false, timeFunConsumed
+			}
+
+			pubKey := tx.Inputs[i].ScriptSig.GetPubKey().ToHexString()
+			sign := tx.Inputs[i].ScriptSig.GetSignature().ToHexString()
+
+			if !ecdsa.Verify(pubKey, sign, hashMesOfTx) {
+				return false, timeFunConsumed
+			}
+
+			utxo, res := txo.GetUtxo(tx.Inputs[i].TxHash, tx.Inputs[i].OutInd)
+			if !res {
+				return false, timeFunConsumed
+			}
+			curVal := utxo.Value
+			//curVal := account.GetBalHashOutInd(tx.Inputs[i].TxHash, tx.Inputs[i].OutInd)
+			inpValue += curVal
+		}
+
+		for i := 0; i < len(tx.Outputs); i++ {
+			outValue += tx.Outputs[i].Value
+		}
+
+		// Checking presence of coins to be spent
+		if inpValue < outValue {
+			return false, timeFunConsumed
+		}
+		return true, timeFunConsumed
+	}
+	return false, timeFunConsumed
+}
+
 func (tvt *TransVerifTime) TransVerifTime() {
 
 	dbController.DbPath = "testing/testData"
@@ -41,8 +82,7 @@ func (tvt *TransVerifTime) TransVerifTime() {
 	log.Printf("Generated %d test utxo\n", len(txo.CoinDatabase))
 
 	tx, _ := testing.GenRandTxs(1, 0, tvt.random)
-	transaction.PrintTransaction(tx[0])
-	//tvt.printResults()
+	transaction.VerifyTransaction(tx[0])
 	dbController.DB.CloseDb()
 	os.RemoveAll(dbController.DbPath)
 }
